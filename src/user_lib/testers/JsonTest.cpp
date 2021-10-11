ba17be75293aeda28b75f3d5708d2eddfc163f5b
yanivcohen1 <yaniv.choen.1@gmail.com>
2021-10-11 11:57:51 +0300

    Add files via upload

---
+++ src/user_lib/testers/JsonTest.cpp
@@ -0,0 +1,136 @@
+#include <ArduinoJson.h>
+#include "Arduino.h"
+
+void json_setup() {
+  // Initialize serial port
+  Serial.begin(115200);
+  while (!Serial) continue;
+
+  // Allocate the JSON document
+  //
+  // Inside the brackets, 200 is the capacity of the memory pool in bytes.
+  // Don't forget to change this value to match your JSON document.
+  // Use arduinojson.org/v6/assistant to compute the capacity.
+  StaticJsonDocument<200> doc_from_json;
+
+  // StaticJsonDocument<N> allocates memory on the stack, it can be
+  // replaced by DynamicJsonDocument which allocates in the heap.
+  //
+  // DynamicJsonDocument doc(200);
+
+  // JSON input string.
+  //
+  // Using a char[], as shown here, enables the "zero-copy" mode. This mode uses
+  // the minimal amount of memory because the JsonDocument stores pointers to
+  // the input buffer.
+  // If you use another type of input, ArduinoJson must copy the strings from
+  // the input to the JsonDocument, so you need to increase the capacity of the
+  // JsonDocument.
+  char json[] =
+      "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+
+  // Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc_from_json, json);
+
+  // Test if parsing succeeds.
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  // Fetch values.
+  //
+  // Most of the time, you can rely on the implicit casts.
+  // In other case, you can do doc["time"].as<long>();
+  const char* sensor = doc_from_json["sensor"];
+  long time = doc_from_json["time"];
+  double latitude = doc_from_json["data"][0];
+  double longitude = doc_from_json["data"][1];
+
+  // Print values.
+  Serial.println(sensor);
+  Serial.println(time);
+  Serial.println(latitude, 6);
+  Serial.println(longitude, 6);
+
+  Serial.println("Changing Sensor");
+
+  doc_from_json["sensor"] = "GPS2"; // update
+
+  // print the update sensore
+  serializeJsonPretty(doc_from_json, Serial);
+  
+  JsonObject sensor1 = doc_from_json["time"];
+  sensor1.remove("time"); // remove
+  // looping all
+  JsonObject documentRoot = doc_from_json.as<JsonObject>();
+  for (JsonPair keyValue : documentRoot) {
+    Serial.print(keyValue.value().as<const char*>());
+    Serial.print(" = ");
+    Serial.println(keyValue.key().c_str());
+  }
+
+  // ***********************************************************************
+  // Allocate the JSON document
+  //
+  // Inside the brackets, 200 is the RAM allocated to this document.
+  // Don't forget to change this value to match your requirement.
+  // Use https://arduinojson.org/v6/assistant to compute the capacity.
+  StaticJsonDocument<200> doc_to_json;
+
+  // StaticJsonObject allocates memory on the stack, it can be
+  // replaced by DynamicJsonDocument which allocates in the heap.
+  //
+  // DynamicJsonDocument  doc(200);
+
+  // Add values in the document
+  //
+  doc_to_json["sensor"] = "gps";
+  doc_to_json["time"] = 1351824120;
+
+  // Add an array.
+  //
+  JsonArray data = doc_to_json.createNestedArray("data");
+  data.add(48.756080);
+  data.add(2.302038);
+
+  // Generate the minified JSON and send it to the Serial port.
+  //
+  Serial.println();
+  serializeJson(doc_to_json, Serial);
+  String ser_str = String("");
+  serializeJson(doc_to_json, ser_str);
+  Serial.println();
+  Serial.print("to string cast: ");
+  Serial.println(ser_str);
+  // The above line prints:
+  // {"sensor":"gps","time":1351824120,"data":[48.756080,2.302038]}
+  // Start a new line
+  Serial.println();
+  // Generate the prettified JSON and send it to the Serial port.
+  serializeJsonPretty(doc_to_json, Serial);
+
+  Serial.print("contain field 'sensor': ");
+  // JsonObject root = doc_to_json.to<JsonObject>();
+  JsonVariant sense = doc_to_json["sensor"];
+  Serial.println(!sense.isNull());
+  if (!sense.isNull()) {
+    Serial.println(sense.as<const char*>());
+  }
+  JsonVariant sense1 = doc_to_json["sensor1"];
+  Serial.println();
+  Serial.print("contain field 'sensor1': ");
+  Serial.println(!sense1.isNull());
+
+  //  **************************************************
+  // nested objects set
+  // doc_to_json["list"][0]["main"]["temp"] = true;
+  // nested object find
+  // const char* sensor = doc_to_json["list"][0]["main"]["temp"];
+  // nested add wifi inside list (list:{wifi: TheBatCave})
+  JsonObject wifi  = doc_to_json["sensor"].createNestedObject("wifi");
+  wifi["SSID"] = "TheBatCave"; // sensor:{wifi: {SSID: TheBatCave}}
+  Serial.println(doc_to_json["sensor"]["wifi"].as<const char*>());
+  serializeJsonPretty(doc_to_json, Serial);
+}
---
+++ src/user_lib/testers/MqttTest.cpp
@@ -0,0 +1,147 @@
+// This example uses an ESP32 Development Board
+// to connect to shiftr.io.
+//
+// You can check on your device after a successful
+// connection here: https://www.shiftr.io/try.
+//
+// by Joël Gähwiler
+// https://github.com/256dpi/arduino-mqtt
+
+#include <WiFi.h>
+extern "C" {
+	#include "freertos/FreeRTOS.h"
+	#include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
+#include "Arduino.h"
+
+namespace Mqtt1
+{
+    #define WIFI_SSID "yourSSID"
+    #define WIFI_PASSWORD "yourpass"
+
+    #define MQTT_HOST IPAddress(192, 168, 1, 10)
+    #define MQTT_PORT 1883
+
+    AsyncMqttClient mqttClient;
+    TimerHandle_t mqttReconnectTimer;
+    TimerHandle_t wifiReconnectTimer;
+
+    void connectToWifi() {
+        Serial.println("Connecting to Wi-Fi...");
+        WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    }
+
+    void connectToMqtt() {
+        Serial.println("Connecting to MQTT...");
+        mqttClient.connect();
+    }
+
+    void WiFiEvent(WiFiEvent_t event) {
+        Serial.printf("[WiFi-event] event: %d\n", event);
+        switch(event) {
+        case SYSTEM_EVENT_STA_GOT_IP:
+            Serial.println("WiFi connected");
+            Serial.println("IP address: ");
+            Serial.println(WiFi.localIP());
+            connectToMqtt();
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            Serial.println("WiFi lost connection");
+            xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+            xTimerStart(wifiReconnectTimer, 0);
+            break;
+        }
+    }
+
+    void onMqttConnect(bool sessionPresent) {
+        Serial.println("Connected to MQTT.");
+        Serial.print("Session present: ");
+        Serial.println(sessionPresent);
+        //qos=2: The broker/client will deliver the message exactly once by using a four step handshake
+        uint16_t packetIdSub = mqttClient.subscribe("test/lol", 2);
+        Serial.print("Subscribing at QoS 2, packetId: ");
+        Serial.println(packetIdSub);
+        // qos=0: The broker/client will deliver the message once, with no confirmation
+        mqttClient.publish("test/lol", 0, true, "test 1");
+        Serial.println("Publishing at QoS 0");
+        // qos=1: The broker/client will deliver the message at least once, with confirmation required
+        uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
+        Serial.print("Publishing at QoS 1, packetId: ");
+        Serial.println(packetIdPub1);
+        //qos=2: The broker/client will deliver the message exactly once by using a four step handshake
+        uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3"); // best
+        Serial.print("Publishing at QoS 2, packetId: ");
+        Serial.println(packetIdPub2);
+    }
+
+    void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+        Serial.println("Disconnected from MQTT.");
+
+        if (WiFi.isConnected()) {
+            xTimerStart(mqttReconnectTimer, 0);
+        }
+    }
+
+    void onMqttSubscribe(uint16_t packetId, uint8_t qos) { //mqttClient.subscribe("test/lol", 2);
+        Serial.println("Subscribe acknowledged.");
+        Serial.print("  packetId: ");
+        Serial.println(packetId);
+        Serial.print("  qos: ");
+        Serial.println(qos);
+    }
+
+    void onMqttUnsubscribe(uint16_t packetId) {
+        Serial.println("Unsubscribe acknowledged.");
+        Serial.print("  packetId: ");
+        Serial.println(packetId);
+    }
+
+    void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+        Serial.println("Publish received.");
+        Serial.print("  topic: ");
+        Serial.println(topic);
+        Serial.print("  qos: ");
+        Serial.println(properties.qos);
+        Serial.print("  dup: ");
+        Serial.println(properties.dup);
+        Serial.print("  retain: ");
+        Serial.println(properties.retain);
+        Serial.print("  len: ");
+        Serial.println(len);
+        Serial.print("  index: ");
+        Serial.println(index);
+        Serial.print("  total: ");
+        Serial.println(total);
+    }
+
+    void onMqttPublish(uint16_t packetId) {
+        Serial.println("Publish acknowledged.");
+        Serial.print("  packetId: ");
+        Serial.println(packetId);
+    }
+
+    void setup() {
+        Serial.begin(115200);
+        Serial.println();
+        Serial.println();
+
+        mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+        wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+
+        WiFi.onEvent(WiFiEvent);
+
+        mqttClient.onConnect(onMqttConnect);
+        mqttClient.onDisconnect(onMqttDisconnect);
+        mqttClient.onSubscribe(onMqttSubscribe);
+        mqttClient.onUnsubscribe(onMqttUnsubscribe);
+        mqttClient.onMessage(onMqttMessage);
+        mqttClient.onPublish(onMqttPublish);
+        mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+
+        connectToWifi();
+    }
+
+    void loop() {
+    }
+}
